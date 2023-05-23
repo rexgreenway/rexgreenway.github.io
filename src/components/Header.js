73cee8a0933abbf08d857136a875aeb429751f26
Rex Greenway <rexgreenway@gmail.com>
2023-05-23 21:29:52 +0100

    Header now atomic component instead of container

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,21 @@
 import styles from './Header.module.css'
 
 import Container from '../containers/Container'
+import Logo from './Logo'
+import Links from '../Links'
+// import NavBar from "../components/NavBar";
 
 
-function Header({ children }) {
+function Header() {
     return (
         <div>
             <Container className={styles.Header}>
-                { children }
+                <div className={styles.Title}>
+                    <Logo />
+                    <h1>Rex Greenway</h1>
+                </div>
+                <Links />
+                {/* <NavBar /> */}
             </Container>
         </div>
     )
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,20 +2,13 @@
 import styles from './Layout.module.css'
 
 import Header from "../components/Header";
-// import NavBar from "../components/NavBar";
-import Title from "../text/Title";
-import Links from '../Links';
 import Footer from '../components/Footer';
 
 
 export default function Layout({ className, children }) {
     return (
         <div className={styles.Layout}>
-            <Header>
-                <Title />
-                <Links />
-                {/* <NavBar /> */}
-            </Header>
+            <Header />
             
             { children }
 
