9c8b7cf096fda06bf47f8cc833b83cb4e30bab26
Rex Greenway <rexgreenway@gmail.com>
2023-05-29 21:49:08 +0100

    cleaner header links implementation

--- src/Links.js
+++ src/Links.js
@@ -1,18 +1,17 @@
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-import styles from './Links.module.css';
-
+import styles from "./Links.module.css";
 
 export default function Links() {
     return (
         <div className={styles.Links}>
-            <a href='https://github.com/RexGreenway/'>
-                <GitHubIcon className={styles.Link}/>
+            <a href="https://github.com/RexGreenway/">
+                <GitHubIcon className={styles.Link} />
             </a>
-            <a href='https://www.linkedin.com/in/rexgreenway/'>
-                <LinkedInIcon className={styles.Link}/>
+            <a href="https://www.linkedin.com/in/rexgreenway/">
+                <LinkedInIcon className={styles.Link} />
             </a>
         </div>
     );
-};
+}
