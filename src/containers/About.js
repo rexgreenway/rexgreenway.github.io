0a4a7f9726d8c27df28650ff39e4d74929fd77fa
Rex Greenway <rexgreenway@gmail.com>
2023-06-03 16:47:14 +0100

    Splitpane about

--- src/containers/About.js
+++ src/containers/About.js
@@ -4,24 +4,37 @@ import HorizontalLine from "../utils/HorizontalLine";
 import Container from "./Container";
 
 import styles from "./About.module.css";
+import SplitPane from "./SplitPane";
 
 function About() {
     return (
-        <Container className={styles.About}>
-            <div>
-                <h2>
-                    <PlaceIcon /> London, UK
-                </h2>
-                <h1>Software Engineer</h1>
-            </div>
-            <HorizontalLine />
-            <p>
-                Specialising in AI Software Development, I have a passion for
-                object oriented elegant programming solutions.
-            </p>
-            <HorizontalLine />
-            <h3>PYTHON - JAVASCRIPT - GCP - K8S - DOCKER - & MORE</h3>
-        </Container>
+        <SplitPane
+            left={
+                <Container className={styles.About}>
+                    <div>
+                        <h2>
+                            <PlaceIcon /> London, UK
+                        </h2>
+                        <h1>Software Engineer</h1>
+                    </div>
+                    <HorizontalLine />
+                    <p>
+                        Specialising in AI Software Development, I have a
+                        passion for object oriented elegant programming
+                        solutions.
+                    </p>
+                </Container>
+            }
+            right={
+                <Container>
+                    <h3>Experience</h3>
+                    <p>Software Engineer @ PwC (Sept. 2021 - Present)</p>
+                    <h3>Education</h3>
+                    <p>MSc Data Science & Artificial Intelligence</p>
+                    <p>BSc Mathematics</p>
+                </Container>
+            }
+        />
     );
 }
 
