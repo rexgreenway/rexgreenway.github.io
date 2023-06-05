b0824f1082483c7f647e1d3228abf3df9f829b58
Rex Greenway <rexgreenway@gmail.com>
2023-06-05 23:07:11 +0100

    About rework

--- src/containers/About.js
+++ src/containers/About.js
@@ -4,66 +4,40 @@ import styles from "./About.module.css";
 
 import HorizontalLine from "../utils/HorizontalLine";
 import Container from "./Container";
-import SplitPane from "./SplitPane";
-import Card from "./Card";
-import Carousel from "../containers/Carousel";
-
-import { ReactComponent as Python } from "../assets/icons/python-plain.svg";
-import { ReactComponent as JS } from "../assets/icons/javascript-plain.svg";
-import { ReactComponent as Docker } from "../assets/icons/docker-plain.svg";
-import { ReactComponent as GCP } from "../assets/icons/googlecloud-plain.svg";
-import { ReactComponent as GO } from "../assets/icons/go-original-wordmark.svg";
-import { ReactComponent as K8s } from "../assets/icons/kubernetes-plain.svg";
 
 function About() {
     return (
-        <>
-            <SplitPane
-                className={styles.About}
-                left={
-                    <Container>
-                        <div>
-                            <h2>
-                                <PlaceIcon /> London, UK
-                            </h2>
-                            <h1>Software Engineer</h1>
-                        </div>
-                        <HorizontalLine />
-                        <p>
-                            Specialising in AI Software Development, I have a
-                            passion for object oriented elegant programming
-                            solutions.
-                        </p>
-                    </Container>
-                }
-                right={
-                    <Card className={styles.EdEx}>
-                        <h3>Experience:</h3>
-                        <div>
-                            <small>AI Software Engineer</small>
-                            <p>PwC (Sept. 2021 - Present)</p>
-                        </div>
+        <Container className={styles.About}>
+            <div className={styles.Main}>
+                <h2>
+                    <PlaceIcon /> London, UK
+                </h2>
+                <h1>Software Engineer </h1>
+                <HorizontalLine />
+                <p>
+                    Specialising in AI Software Development, I have a passion
+                    for object oriented elegant programming solutions.
+                </p>
+                <HorizontalLine />
 
-                        <h3>Education:</h3>
-                        <div>
-                            <small>University of Liverpool</small>
-                            <p>MSc Data Science & AI - Distinction</p>
-                            <br />
-                            <small>University of Liverpool</small>
-                            <p>BSc Mathematics - 1st Class</p>
-                        </div>
-                    </Card>
-                }
-            />
-            <Carousel>
-                <Python className={styles.Logo} />
-                <Docker className={styles.Logo} />
-                <JS className={styles.Logo} />
-                <GCP className={styles.Logo} />
-                <GO className={styles.Logo} />
-                <K8s className={styles.Logo} />
-            </Carousel>
-        </>
+                <h4>PYTHON - GO - GCP - K8S - DOCKER - JAVASCRIPT</h4>
+            </div>
+            <div className={styles.ExEd}>
+                <div>
+                    <h5>AI Software Engineer, PwC</h5>
+                    <p>Sept. 2021 - Present</p>
+                </div>
+                <br />
+                <div>
+                    <h5>MSc Data Science & AI</h5>
+                    <p>Distinction</p>
+                </div>
+                <div>
+                    <h5>BSc Mathematics</h5>
+                    <p>1st Class</p>
+                </div>
+            </div>
+        </Container>
     );
 }
 
