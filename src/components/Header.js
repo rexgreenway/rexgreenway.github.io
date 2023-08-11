be14c145ed948985cff570f03626bfc6b90633f3
Rex Greenway <rexgreenway@gmail.com>
2023-08-11 17:04:01 +0100

    Header / Footer overhaul

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,39 @@
-import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
+
+import { ReactComponent as RexLogo } from "../assets/R-Logo.svg";
 
 import Container from "../containers/Container";
-import Logo from "./Logo";
-import Links from "../Links";
-// import NavBar from "../components/NavBar";
+
+import styles from "./Header.module.css";
+import HorizontalLine from "./HorizontalLine";
+
+// NavLink Wrapper for CSS Module Use
+function HeaderLink({ to, children }) {
+    return (
+        <NavLink
+            to={to}
+            className={({ isActive }) => (isActive ? styles.active : "")}
+        >
+            {children}
+        </NavLink>
+    );
+}
 
 function Header() {
     return (
-        <Container className={styles.Header}>
-            <div className={styles.Title}>
-                <Logo />
-                <h1>Rex Greenway</h1>
-            </div>
-            <Links />
-            {/* <NavBar /> */}
-        </Container>
+        <div id="header">
+            <Container className={styles.Header}>
+                {/* TODO: Make LOGO/TITLE Clickable */}
+                <RexLogo className={styles.Logo} />
+                <h1 className={styles.Title}>Rex Greenway</h1>
+                <nav className={styles.NavBar}>
+                    <HeaderLink to="/">Home</HeaderLink>
+                    <HeaderLink to="/projects">Projects</HeaderLink>
+                    <HeaderLink to="/about">About</HeaderLink>
+                </nav>
+            </Container>
+            <HorizontalLine />
+        </div>
     );
 }
 
