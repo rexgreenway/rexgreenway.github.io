79c8be053b6025437eb05135c2fff8ead61a63c4
Rex Greenway <rexgreenway@gmail.com>
2023-08-11 17:02:37 +0100

    Proper use of react router with Outlet and App

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,32 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App'
-
+import App from "./App";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import AboutPage from "./pages/About";
 
 const router = createBrowserRouter([
     {
         path: "/",
+        element: <App />,
         errorElement: <h1>Something Went Wrong</h1>,
         children: [
             {
                 index: true,
-                element: <App />
+                element: <Home />,
             },
-        ]
+            {
+                path: "projects",
+                element: <Projects />,
+            },
+            {
+                path: "about",
+                element: <AboutPage />,
+            },
+        ],
     },
 ]);
 
-
 export default function Router() {
-    return <RouterProvider router={router}/>;
-};
+    return <RouterProvider router={router} />;
+}
