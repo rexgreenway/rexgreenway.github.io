4310ce329ed79e74c95bc6101e1408cb956a0317
Rex Greenway <74911207+RexGreenway@users.noreply.github.com>
2024-02-04 14:53:54 +0000

    About Update - CV inclusion (#15)
    * updated about frontpage about section
    
    * create open PDF button component
    
    * add download PDF to website

--- src/components/About.js
+++ src/components/About.js
@@ -2,27 +2,30 @@ import PlaceIcon from "@mui/icons-material/Place";
 
 import styles from "./About.module.css";
 
-import HorizontalLine from "./HorizontalLine";
 import Container from "../containers/Container";
+import { PDFLink } from "./CustomLinks";
+
+import PDF from "../documents/2024-02-04-CV.pdf";
 
 function About() {
-    return (
-        <Container className={styles.About}>
-            <div className={styles.Main}>
-                <h2>
-                    <PlaceIcon /> London, UK
-                </h2>
-                <h1>Software Engineer </h1>
-                <p>
-                    Specialising in AI Software Development, I have a passion
-                    for object oriented elegant programming solutions.
-                </p>
-                <HorizontalLine />
+  return (
+    <Container className={styles.About}>
+      <div className={styles.Main}>
+        <h2>
+          <PlaceIcon /> London, UK
+        </h2>
+        <h1>Software Engineer </h1>
+        <p>
+          A software engineer passionate about implementing elegant software
+          solutions across the entire stack, from design to deployment.
+        </p>
+        <br />
 
-                <h4>PYTHON - GO - GCP - K8S - DOCKER - JAVASCRIPT</h4>
-            </div>
-        </Container>
-    );
+        {/* CV Download */}
+        <PDFLink pdfUrl={PDF}>Download CV</PDFLink>
+      </div>
+    </Container>
+  );
 }
 
 export default About;
