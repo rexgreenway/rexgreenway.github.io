d39ec8f0e794aa668eafad4047e5939c60c3b16b
Rex Greenway <rexgreenway@gmail.com>
2024-02-04 14:51:38 +0000

    create open PDF button component

--- src/components/CustomLinks.js
+++ src/components/CustomLinks.js
@@ -5,23 +5,40 @@ import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRound
 import styles from "./CustomLinks.module.css";
 
 export function ExternalLink({ to, children }) {
-    return (
-        <Link
-            className={`${styles.Links} ${styles.ExternalLink}`}
-            to={to}
-            target="_blank"
-        >
-            {children}
-            <OpenInNewIcon />
-        </Link>
-    );
+  return (
+    <Link
+      className={`${styles.Links} ${styles.ExternalLink}`}
+      to={to}
+      target="_blank"
+    >
+      {children}
+      <OpenInNewIcon />
+    </Link>
+  );
+}
+
+export function PDFLink({ pdfUrl, children }) {
+  const openPDF = () => {
+    window.open(pdfUrl, "_blank");
+  };
+
+  return (
+    <button
+      className={`${styles.Links} ${styles.ExternalLink} ${styles.Button}`}
+      onClick={openPDF}
+      target="_blank"
+    >
+      {children}
+      <OpenInNewIcon />
+    </button>
+  );
 }
 
 export function SeeMore({ to, children }) {
-    return (
-        <NavLink className={`${styles.Links} ${styles.SeeMore}`} to={to}>
-            {children}
-            <ArrowForwardIosRoundedIcon />
-        </NavLink>
-    );
+  return (
+    <NavLink className={`${styles.Links} ${styles.SeeMore}`} to={to}>
+      {children}
+      <ArrowForwardIosRoundedIcon />
+    </NavLink>
+  );
 }
