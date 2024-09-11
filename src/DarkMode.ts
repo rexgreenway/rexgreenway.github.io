3b55d81915a974096335a6eaeb3c0fa68dd596f6
Rex <rexgreenway@proton.me>
2024-09-11 23:02:03 +0100

    theme enum

--- src/DarkMode.ts
+++
@@ -1,7 +0,0 @@
-export const setDark = () => {
-  document.documentElement.setAttribute("data-theme", "dark");
-};
-
-export const setLight = () => {
-  document.documentElement.setAttribute("data-theme", "light");
-};
