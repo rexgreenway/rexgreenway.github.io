a5993bd3a6278e07384d28ccddb0fee9385ec391
Rex <rexgreenway@proton.me>
2024-09-11 22:45:19 +0100

    add css variables and themes

---
+++ src/DarkMode.ts
@@ -0,0 +1,7 @@
+export const setDark = () => {
+  document.documentElement.setAttribute("data-theme", "dark");
+};
+
+export const setLight = () => {
+  document.documentElement.setAttribute("data-theme", "light");
+};
