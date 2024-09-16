e4b110bc558dd3ab97326875283cec5c02964c37
Rex Greenway <rex.greenway@neo4j.com>
2024-09-16 14:14:18 +0100

    streamline footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,24 @@
 import Container from "../containers/Container";
-import Links from "../Links";
 import HorizontalLine from "./HorizontalLine";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <div id="footer">
       <HorizontalLine />
       <Container>
-        <Links />
+        <div className={styles.Links}>
+          <a href="https://github.com/RexGreenway/">
+            <GitHubIcon className={styles.Link} />
+          </a>
+          <a href="https://www.linkedin.com/in/rexgreenway/">
+            <LinkedInIcon className={styles.Link} />
+          </a>
+        </div>
       </Container>
     </div>
   );
