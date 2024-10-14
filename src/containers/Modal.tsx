58498957faebf293be2017c1cad065a2e8fef8d4
Rex <rexgreenway@proton.me>
2024-10-14 18:40:21 +0100

    Modal and Image Modal components

--- src/containers/Modal.tsx
+++ src/containers/Modal.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 import styles from "./Modal.module.css";
 
@@ -8,15 +9,47 @@ interface ModalProps {
 }
 
 const Modal = ({ close, children }: ModalProps) => {
+  // Close on Escape Key Press
+  useEffect(() => {
+    const handleEscKey = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        close();
+      }
+    };
+    document.addEventListener("keydown", handleEscKey);
+    return () => {
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, [close]);
+
   return (
-    <div className={styles.Modal}>
-      <div className={styles.ModelContent}>
-        <h1>MODAL</h1>
-        <button onClick={close}>CLOSE</button>
-        {children}
-      </div>
+    <div className={styles.ModalBackground}>
+      <CloseRoundedIcon
+        fontSize="large"
+        className={styles.CloseButton}
+        onClick={close}
+      />
+      <div className={styles.Model}>{children}</div>
     </div>
   );
 };
 
-export default Modal;
+const ImageModal = ({
+  src,
+  isOpen,
+  close,
+}: {
+  src: string;
+  isOpen: boolean;
+  close: () => void;
+}) => {
+  return (
+    isOpen && (
+      <Modal close={close}>
+        <img className={styles.ImageModal} src={src} />
+      </Modal>
+    )
+  );
+};
+
+export { Modal, ImageModal };
