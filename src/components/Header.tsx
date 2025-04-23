1f575c5418c5f06f18eef2b872802acb05863668
Rex <rexgreenway@proton.me>
2025-04-23 20:37:34 +0100

    simple header variant

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,79 +1,84 @@
 import { NavLink } from "react-router-dom";
+import { capitalize } from "@mui/material";
 
 import RexLogo from "../assets/R-Logo.svg?react";
 
 import { CustomRouteObject } from "../router/routes";
 
 import Container from "../containers/Container";
-import HorizontalLine from "./HorizontalLine";
 
 import styles from "./Header.module.css";
-import { capitalize } from "@mui/material";
 
-const Header = ({
-  homeTitle,
-  navLinks,
-  adjacentLinks,
-}: {
+interface HeaderProps {
   homeTitle: string;
   navLinks?: CustomRouteObject;
   adjacentLinks?: CustomRouteObject[];
-}) => {
-  console.log(adjacentLinks);
+}
 
+const Header = ({ homeTitle, navLinks, adjacentLinks }: HeaderProps) => {
   return (
-    <div id="header">
-      <Container className={styles.Header}>
-        {/* LOGO */}
-        <NavLink to="" className={styles.Logo}>
-          <RexLogo />
+    <Container id="header" className={styles.Header}>
+      {/* LOGO : Separate to Title as to sit next to navLinks */}
+      <NavLink to="" className={styles.Logo}>
+        <RexLogo />
+      </NavLink>
+
+      {/* TITLE / NAME : Nested in div to control link styling */}
+      <div className={styles.Title}>
+        <NavLink to="">
+          <h1>Rex Greenway</h1>
         </NavLink>
+      </div>
 
-        {/* TITLE / NAME */}
-        <div className={styles.Title}>
-          <NavLink to="">
-            <h1>Rex Greenway</h1>
-          </NavLink>
-        </div>
+      {/* ADJACENT LINKS */}
+      <div className={styles.Adjacent}>
+        {adjacentLinks?.map((l) => {
+          return (
+            <NavLink
+              to={l.path || ""}
+              key={l.path}
+              title={capitalize(l.path?.slice(1)!)}
+            >
+              {l.icon}
+            </NavLink>
+          );
+        })}
+      </div>
 
-        {/* ADJACENT LINKS */}
-        <div className={styles.Adjacent}>
-          {adjacentLinks?.map((l) => {
+      {/* NAVIGATION: Based on passed contextual route hierarchy */}
+      <nav className={styles.NavBar}>
+        {navLinks?.children?.map((k) => {
+          // Only include nav links to non dynamic segments
+          if (!k.path?.includes(":")) {
             return (
               <NavLink
-                to={l.path || ""}
-                key={l.path}
-                title={capitalize(l.path?.slice(1)!)}
+                className={({ isActive }) =>
+                  isActive ? styles.active : styles.inactive
+                }
+                key={k.path || "home"}
+                to={k.path || ""}
+                end
               >
-                {l.icon}
+                {k.path || homeTitle}
               </NavLink>
             );
-          })}
-        </div>
+          }
+        })}
+      </nav>
+    </Container>
+  );
+};
 
-        {/* NAVIGATION: Based on passed contextual route hierarchy */}
-        <nav className={styles.NavBar}>
-          {navLinks?.children?.map((k) => {
-            // Only include nav links to non dynamic segments
-            if (!k.path?.includes(":")) {
-              return (
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive ? styles.active : styles.inactive
-                  }
-                  key={k.path || "home"}
-                  to={k.path || ""}
-                  end
-                >
-                  {k.path || homeTitle}
-                </NavLink>
-              );
-            }
-          })}
-        </nav>
-      </Container>
-      <HorizontalLine />
-    </div>
+export const SimpleHeader = () => {
+  return (
+    <Container id="header" className={styles.SimpleHeader}>
+      <NavLink to="" className={styles.Logo}>
+        <RexLogo />
+      </NavLink>
+      <NavLink to="">
+        <h1>Rex Greenway</h1>
+      </NavLink>
+    </Container>
   );
 };
 
