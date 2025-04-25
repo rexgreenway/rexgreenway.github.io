e06740bf0cb29c6b93ed4ed3066ff791dcc69707
Rex <rexgreenway@proton.me>
2025-04-25 17:01:41 +0100

    api calls updated for new api backend

--- src/api/rex-api/fetchPhotography.ts
+++ src/api/rex-api/fetchPhotography.ts
@@ -1,79 +1,49 @@
-import { Album, Image } from "@mui/icons-material";
-
+// CONSTANTS
 // const API_URL = "http://127.0.0.1:8000";
 const API_URL = "https://rex-api-505972842640.europe-west2.run.app";
+
+const THUMBNAIL_BUCKET = "rex-thumbnails";
 
 // RESPONSE TYPES
 
 export interface Image {
-  path: string;
   url: string;
-}
-
-export interface Album {
-  name: string;
-  path: string;
-  images: Image[];
+  name?: string;
+  size?: string;
 }
 
 export enum Size {
-  SMALL = "small",
+  THUMBNAIL = "thumbnail",
   LARGE = "large",
 }
+
+// UTILITY FUNCS
+export const getThumbnailURL = (image_name: string): string => {
+  return `https://storage.googleapis.com/${THUMBNAIL_BUCKET}/${image_name}`;
+};
 
 // REQUESTS
 
 /**
- * Fetches an album.
- *
- * @param {string} album_name
- *
- * @returns {Album} The desired album.
- */
-export async function getAlbum(album_name: string): Promise<Album> {
-  const encoded_name = encodeURI(album_name);
-  const url = `${API_URL}/photography/${encoded_name}`;
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(
-        `failed to fetch album '${album_name}' | status: ${response.status}`
-      );
-    }
-    return await response.json();
-  } catch (error) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else {
-      console.log("unknown error: ", error);
-    }
-    throw error;
-  }
-}
-
-/**
  * Fetches a photo from a given album.
  *
- * @param {string} album_name
  * @param {string} image_name
  * @param {Size} [size=Size.LARGE] Optional
  *
  * @returns {Image} The desired photo.
  */
 export async function getPhoto(
-  album_name: string,
   image_name: string,
   size: Size = Size.LARGE
 ): Promise<Image> {
-  const encoded_path = encodeURI(`${album_name}/${image_name}?size=${size}`);
+  const encoded_path = encodeURI(`${image_name}?size=${size}`);
   const url = `${API_URL}/photography/${encoded_path}`;
 
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(
-        `failed to fetch photo '${album_name}/${image_name}' | status: ${response.status}`
+        `failed to fetch photo '${image_name}' | status: ${response.status}`
       );
     }
     return await response.json();
