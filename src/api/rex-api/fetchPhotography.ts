326f93b3f9ce5bceec8dbe19299c0388fe1f262a
Rex <74911207+rexgreenway@users.noreply.github.com>
2025-04-28 23:13:57 +0100

    Photography v2 (#47)
    * background color transition
    
    * system Theme and setting rework
    
    * new links page
    
    * api calls updated for new api backend
    
    * update main albums page for public thumbnails
    
    * public thumbnails for album pages too.
    
    * add wake up call
    
    * add albums

--- src/api/rex-api/fetchPhotography.ts
+++ src/api/rex-api/fetchPhotography.ts
@@ -1,53 +1,55 @@
-import { Album, Image } from "@mui/icons-material";
+import data from "../../assets/albums.json";
 
+// CONSTANTS
 // const API_URL = "http://127.0.0.1:8000";
 const API_URL = "https://rex-api-505972842640.europe-west2.run.app";
+
+const THUMBNAIL_BUCKET = "rex-thumbnails";
+
+// ASSETS
+export const ALBUMS: { [key: string]: Album } = data;
+
+interface PhotoInfo {
+  film_stock?: string;
+}
+
+interface Album {
+  name: string;
+  thumbnail: string;
+  photos: { [name: string]: PhotoInfo };
+}
 
 // RESPONSE TYPES
 
 export interface Image {
-  path: string;
   url: string;
-}
-
-export interface Album {
-  name: string;
-  path: string;
-  images: Image[];
+  name?: string;
+  size?: string;
 }
 
 export enum Size {
-  SMALL = "small",
+  THUMBNAIL = "thumbnail",
   LARGE = "large",
 }
+
+// UTILITY FUNCS
+export const getThumbnailURL = (image_name: string): string => {
+  return `https://storage.googleapis.com/${THUMBNAIL_BUCKET}/${image_name}`;
+};
 
 // REQUESTS
 
 /**
- * Fetches an album.
- *
- * @param {string} album_name
- *
- * @returns {Album} The desired album.
+ * Wake's up the API.
  */
-export async function getAlbum(album_name: string): Promise<Album> {
-  const encoded_name = encodeURI(album_name);
-  const url = `${API_URL}/photography/${encoded_name}`;
-
+export async function wakeUp() {
   try {
-    const response = await fetch(url);
+    const response = await fetch(`${API_URL}/`);
     if (!response.ok) {
-      throw new Error(
-        `failed to fetch album '${album_name}' | status: ${response.status}`
-      );
+      throw new Error(`api did not wake up!`);
     }
-    return await response.json();
   } catch (error) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else {
-      console.log("unknown error: ", error);
-    }
+    console.log(error);
     throw error;
   }
 }
@@ -55,25 +57,23 @@ export async function getAlbum(album_name: string): Promise<Album> {
 /**
  * Fetches a photo from a given album.
  *
- * @param {string} album_name
  * @param {string} image_name
  * @param {Size} [size=Size.LARGE] Optional
  *
  * @returns {Image} The desired photo.
  */
 export async function getPhoto(
-  album_name: string,
   image_name: string,
   size: Size = Size.LARGE
 ): Promise<Image> {
-  const encoded_path = encodeURI(`${album_name}/${image_name}?size=${size}`);
+  const encoded_path = encodeURI(`${image_name}?size=${size}`);
   const url = `${API_URL}/photography/${encoded_path}`;
 
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(
-        `failed to fetch photo '${album_name}/${image_name}' | status: ${response.status}`
+        `failed to fetch photo '${image_name}' | status: ${response.status}`
       );
     }
     return await response.json();
--- src/pages/portfolio/Photography.tsx
+++ src/pages/portfolio/Photography.tsx
@@ -1,76 +1,29 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { getPhoto, Image, Size } from "../../api/rex-api/fetchPhotography";
+import {
+  ALBUMS,
+  getThumbnailURL,
+  wakeUp,
+} from "../../api/rex-api/fetchPhotography";
 
 import { Thumbnail, ThumbnailGrid } from "../../containers/Thumbnail";
 
-import Collection from "./Collection";
-import data from "../../assets/albums.json";
-
-const COLLECTIONS: { [key: string]: Collection } = data;
-
-const CollectionThumbnail = ({
-  url,
-  displayName,
-  thumbnailAlbum,
-  thumbnailImage,
-}: {
-  url: string;
-  displayName?: string;
-  thumbnailAlbum?: string;
-  thumbnailImage?: string;
-}) => {
+const Photography = () => {
   const navigate = useNavigate();
 
-  const [thumbnailSrc, setThumbnailSrc] = useState<string>("");
+  // Call API to wake up on navigation to Portfolio Page.
+  wakeUp();
 
-  useEffect(() => {
-    if (thumbnailAlbum && thumbnailImage) {
-      getPhoto(thumbnailAlbum, thumbnailImage, Size.SMALL).then(
-        (photo: Image) => {
-          setThumbnailSrc(photo.url);
-        }
-      );
-    } else {
-      setThumbnailSrc(
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fe0.pxfuel.com%2Fwallpapers%2F464%2F518%2Fdesktop-wallpaper-red-background-red-screen.jpg&f=1&nofb=1&ipt=e616183af72aca7d643d24b40551f571552145836951b3a0c9999d3c723d7cc2&ipo=images"
-      );
-    }
-  }, [thumbnailAlbum, thumbnailImage]);
-
-  return (
-    <Thumbnail
-      imageSrc={thumbnailSrc}
-      title={displayName}
-      onClick={() => {
-        navigate(url);
-      }}
-    />
-  );
-};
-
-const Photography = () => {
-  const collections = Object.keys(COLLECTIONS);
   return (
     <ThumbnailGrid>
-      {collections.map((c) => {
-        const collectionName = COLLECTIONS[c].name;
-        const collectionThumbnail = COLLECTIONS[c].thumbnail;
-        return (
-          <CollectionThumbnail
-            key={c}
-            url={c}
-            displayName={collectionName}
-            thumbnailAlbum={
-              collectionThumbnail && collectionThumbnail.album_name
-            }
-            thumbnailImage={
-              collectionThumbnail && collectionThumbnail.image_name
-            }
-          />
-        );
-      })}
+      {Object.entries(ALBUMS).map(([key, album]) => (
+        <Thumbnail
+          key={key}
+          imageSrc={getThumbnailURL(album.thumbnail)}
+          title={album.name}
+          onClick={() => navigate(key)}
+        />
+      ))}
     </ThumbnailGrid>
   );
 };
