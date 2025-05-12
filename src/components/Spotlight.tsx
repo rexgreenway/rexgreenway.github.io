21c62b101500b58c6b0a7e3c1ba89ac0ed0ffe55
Rex <rexgreenway@proton.me>
2025-05-12 11:13:23 +0100

    fix spotlight ui gap

--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -1,4 +1,3 @@
-import Container from "../containers/Container";
 import { CardWithImage } from "../containers/Card";
 import Lattice from "../assets/PolyLatLib-Header.png";
 import { ExternalLink } from "./CustomLinks";
@@ -8,18 +7,20 @@ import styles from "./Spotlight.module.css";
 
 export default function Spotlight() {
   return (
-    <Container className={styles.Spotlight}>
-      <CardWithImage src={Lattice} alt="Geometric Lattice">
-        <h2>PolyLatLib</h2>
-        <HorizontalLine />
-        <p>A Python library for the generation and manipulation of Lattices.</p>
-        <div className={styles.Links}>
-          <ExternalLink
-            to="https://github.com/RexGreenway/PolyLatLib"
-            text="GitHub"
-          />
-        </div>
-      </CardWithImage>
-    </Container>
+    <CardWithImage
+      src={Lattice}
+      alt="Geometric Lattice"
+      className={styles.Spotlight}
+    >
+      <h2>PolyLatLib</h2>
+      <HorizontalLine />
+      <p>A Python library for the generation and manipulation of Lattices.</p>
+      <div className={styles.Links}>
+        <ExternalLink
+          to="https://github.com/RexGreenway/PolyLatLib"
+          text="GitHub"
+        />
+      </div>
+    </CardWithImage>
   );
 }
