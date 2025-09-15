fb47414f612087394fff7af96731fb4e46369a0a
Rex Greenway <rex.greenway@neo4j.com>
2025-09-15 22:03:56 +0100

    add readme preview popup modal

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 import Card from "../containers/Card";
 import HorizontalLine from "./HorizontalLine";
+import ReadMeModal from "./ReadMeModal";
 import { ExternalLink, PagesLink } from "./CustomLinks";
 import { Tags } from "./Tags";
+
+import ArticleIcon from "@mui/icons-material/Article";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
 
 import styles from "./Project.module.css";
 
@@ -30,25 +36,51 @@ export default function Project({
   topics,
   has_pages,
 }: ProjectProps) {
-  return (
-    <Card className={styles.Project}>
-      <div>
-        <ProjectTitle text={name} />
-        <HorizontalLine />
-        <Tags languages={language} tags={topics} />
-        <ProjectDescription text={description} />
-      </div>
+  const [readMeModal, setReadMeModal] = useState<boolean>(false);
 
-      <div className={styles.Links}>
-        {/* Could instead utilise 2 routers a hash router and a not hash router? */}
-        {has_pages && name !== "rexgreenway.github.io" && (
-          <PagesLink
-            to={`https://rexgreenway.github.io/${name}/`}
-            text="Website"
-          />
-        )}
-        <ExternalLink to={html_url} text="GitHub" />
-      </div>
-    </Card>
+  return (
+    <>
+      <Card className={styles.Project}>
+        <div>
+          <div className={styles.ProjectHeader}>
+            <ProjectTitle text={name} />
+            {name !== "rexgreenway" && (
+              <a
+                title="README"
+                className={styles.IconLink}
+                onClick={() => setReadMeModal(true)}
+              >
+                <span className={styles.IconStack}>
+                  <ArticleOutlinedIcon className={styles.IconOutlined} />
+                  <ArticleIcon className={styles.IconFilled} />
+                </span>
+              </a>
+            )}
+          </div>
+          <HorizontalLine />
+          <Tags languages={language} tags={topics} />
+          <ProjectDescription text={description} />
+        </div>
+
+        <div className={styles.Links}>
+          {/* Could instead utilise 2 routers a hash router and a not hash router? */}
+          {has_pages && name !== "rexgreenway.github.io" && (
+            <PagesLink
+              to={`https://rexgreenway.github.io/${name}/`}
+              text="Website"
+            />
+          )}
+          <ExternalLink to={html_url} text="GitHub" />
+        </div>
+      </Card>
+
+      {readMeModal && (
+        <ReadMeModal
+          repo={name}
+          isOpen={readMeModal}
+          close={() => setReadMeModal(false)}
+        />
+      )}
+    </>
   );
 }
