7e42720bb2a8112212bf87435abf170aa0381af1
Rex <rexgreenway@proton.me>
2026-04-22 16:34:18 +0100

    update api url

--- src/api/rex-api/model.ts
+++ src/api/rex-api/model.ts
@@ -1,6 +1,6 @@
 // CONSTANTS
-export const API_URL = "http://127.0.0.1:8080";
-// const API_URL = "https://rex-api-505972842640.europe-west2.run.app";
+// export const API_URL = "http://127.0.0.1:8080";
+export const API_URL = "https://rex-api-505972842640.europe-west2.run.app";
 
 export const THUMBNAIL_BUCKET = "rex-thumbnails";
 
